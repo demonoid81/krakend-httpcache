048fd12aafba003f8389a4efc501d3dd425af1fd
DemonoiD <38601394+demonoid81@users.noreply.github.com>
2023-07-10 19:43:05 +0300

    add redis

---
+++ http.go
@@ -0,0 +1,96 @@
+package httpcache
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/demonoid81/krakend-httpcache/httpcache"
+	"github.com/redis/go-redis/v9"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/luraproject/lura/v2/config"
+	"github.com/luraproject/lura/v2/transport/http/client"
+)
+
+type Cache interface {
+	// Get returns the []byte representation of a cached response and a bool
+	// set to true if the value isn't empty
+	Get(key string) (responseBytes []byte, ok bool)
+	// Set stores the []byte representation of a response against a key
+	Set(key string, responseBytes []byte)
+	// Delete removes the value associated with the key
+	Delete(key string)
+}
+
+// Namespace is the key to use to store and access the custom config data
+const Namespace = "github.com/demonoid81/krakend-httpcache"
+
+var redisClient *redis.Client
+var ttl time.Duration
+
+func init() {
+	redisURL, exists := os.LookupEnv("REDIS_CACHE")
+	if exists {
+		panic("Redis url is not set")
+	}
+	redisPassw := os.Getenv("REDIS_PASSWORD")
+
+	redisDB := 0
+	if dbs, exists := os.LookupEnv("REDIS_DB"); exists {
+		db, err := strconv.Atoi(dbs)
+		if err != nil {
+			panic(err)
+		}
+		redisDB = db
+	}
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisURL,
+		Password: redisPassw, // no password set
+		DB:       redisDB,    // use default DB
+	})
+
+	redisClient = rdb
+
+	ttls := os.Getenv("DEFAULT_TTL")
+	ttli, err := strconv.Atoi(ttls)
+	if err != nil {
+		panic(err)
+	}
+	ttl = time.Duration(ttli)
+}
+
+// NewHTTPClient creates a HTTPClientFactory using an in-memory-cached http client
+func NewHTTPClient(cfg *config.Backend, nextF client.HTTPClientFactory) client.HTTPClientFactory {
+	raw, ok := cfg.ExtraConfig[Namespace]
+	if !ok {
+		return nextF
+	}
+
+	if b, err := json.Marshal(raw); err == nil {
+		var opts options
+		if err := json.Unmarshal(b, &opts); err == nil && opts.TTL > 0 {
+			ttl = opts.TTL
+		}
+	}
+
+	cache := httpcache.NewMemoryCache(redisClient, ttl)
+
+	return func(ctx context.Context) *http.Client {
+		httpClient := nextF(ctx)
+		return &http.Client{
+			Transport: &httpcache.Transport{
+				Transport: httpClient.Transport,
+				Cache:     cache,
+			},
+			CheckRedirect: httpClient.CheckRedirect,
+			Jar:           httpClient.Jar,
+			Timeout:       httpClient.Timeout,
+		}
+	}
+}
+
+type options struct {
+	TTL time.Duration `json:"ttl"`
+}
---
+++ httpcache/httpcache.go
@@ -0,0 +1,287 @@
+package httpcache
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+)
+
+const (
+	XFromCache = "X-From-Cache"
+)
+
+// A Cache interface is used by the Transport to store and retrieve responses.
+type Cache interface {
+	// Get returns the []byte representation of a cached response and a bool
+	// set to true if the value isn't empty
+	Get(key string) (responseBytes []byte, ok bool)
+	// Set stores the []byte representation of a response against a key
+	Set(key string, responseBytes []byte)
+	// Delete removes the value associated with the key
+	Delete(key string)
+}
+
+// cacheKey returns the cache key for req.
+func cacheKey(req *http.Request) string {
+	if req.Method == http.MethodGet {
+		return req.URL.String()
+	} else {
+		return req.Method + " " + req.URL.String()
+	}
+}
+
+func CachedResponse(c Cache, req *http.Request) (resp *http.Response, err error) {
+	cachedVal, ok := c.Get(cacheKey(req))
+	if !ok {
+		return
+	}
+
+	b := bytes.NewBuffer(cachedVal)
+	return http.ReadResponse(bufio.NewReader(b), req)
+}
+
+// MemoryCache is an implemtation of Cache that stores responses in an in-memory map.
+type MemoryCache struct {
+	Client *redis.Client
+	TTL    time.Duration
+}
+
+// Get returns the []byte representation of the response and true if present, false if not
+func (mc *MemoryCache) Get(key string) (resp []byte, ok bool) {
+	res, err := mc.Client.Get(context.Background(), key).Bytes()
+	if err != nil {
+		if err == redis.Nil {
+			return nil, false
+		} else {
+			fmt.Println(fmt.Errorf("error working with Redis: %v", err))
+			return nil, false
+		}
+	}
+	return res, true
+}
+
+// Set saves response resp to the cache with key
+func (mc *MemoryCache) Set(key string, resp []byte) {
+	mc.Client.SetNX(context.Background(), key, resp, mc.TTL)
+}
+
+// Delete removes key from the cache
+func (mc *MemoryCache) Delete(key string) {
+	mc.Client.Del(context.Background(), key)
+}
+
+// NewMemoryCache returns a new Cache that will store items in an in-memory map
+func NewMemoryCache(client *redis.Client, ttl time.Duration) *MemoryCache {
+	c := &MemoryCache{client, ttl}
+	return c
+}
+
+// cachingReadCloser is a wrapper around ReadCloser R that calls OnEOF
+// handler with a full copy of the content read from R when EOF is
+// reached.
+type cachingReadCloser struct {
+	// Underlying ReadCloser.
+	R io.ReadCloser
+	// OnEOF is called with a copy of the content of R when EOF is reached.
+	OnEOF func(io.Reader)
+
+	buf bytes.Buffer // buf stores a copy of the content of R.
+}
+
+// Read reads the next len(p) bytes from R or until R is drained. The
+// return value n is the number of bytes read. If R has no data to
+// return, err is io.EOF and OnEOF is called with a full copy of what
+// has been read so far.
+func (r *cachingReadCloser) Read(p []byte) (n int, err error) {
+	n, err = r.R.Read(p)
+	r.buf.Write(p[:n])
+	if err == io.EOF || n < len(p) {
+		r.OnEOF(bytes.NewReader(r.buf.Bytes()))
+	}
+	return n, err
+}
+
+func (r *cachingReadCloser) Close() error {
+	return r.R.Close()
+}
+
+// Transport is an implementation of http.RoundTripper that will return values from a cache
+// where possible (avoiding a network request) and will additionally add validators (etag/if-modified-since)
+// to repeated requests allowing servers to return 304 / Not Modified
+type Transport struct {
+	// The RoundTripper interface actually used to make requests
+	// If nil, http.DefaultTransport is used
+	Transport http.RoundTripper
+	Cache     Cache
+	// If true, responses returned from the cache will be given an extra header, X-From-Cache
+	MarkCachedResponses bool
+}
+
+// NewTransport returns a new Transport with the
+// provided Cache implementation and MarkCachedResponses set to true
+func NewTransport(c Cache) *Transport {
+	return &Transport{Cache: c, MarkCachedResponses: true}
+}
+
+// Client returns an *http.Client that caches responses.
+func (t *Transport) Client() *http.Client {
+	return &http.Client{Transport: t}
+}
+
+// RoundTrip takes a Request and returns a Response
+//
+// If there is a fresh Response already in cache, then it will be returned without connecting to
+// the server.
+//
+// If there is a stale Response, then any validators it contains will be set on the new request
+// to give the server a chance to respond with NotModified. If this happens, then the cached Response
+// will be returned.
+func (t *Transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	cacheKey := cacheKey(req)
+	cacheable := (req.Method == "GET" || req.Method == "HEAD") && req.Header.Get("range") == ""
+	var cachedResp *http.Response
+	if cacheable {
+		cachedResp, err = CachedResponse(t.Cache, req)
+	} else {
+		// Need to invalidate an existing value
+		t.Cache.Delete(cacheKey)
+	}
+
+	transport := t.Transport
+	if transport == nil {
+		transport = http.DefaultTransport
+	}
+
+	if cacheable && cachedResp != nil && err == nil {
+		if t.MarkCachedResponses {
+			cachedResp.Header.Set(XFromCache, "1")
+		}
+
+		resp, err = transport.RoundTrip(req)
+		if err == nil && req.Method == "GET" && resp.StatusCode == http.StatusNotModified {
+			// Replace the 304 response with the one from cache, but update with some new headers
+			endToEndHeaders := getEndToEndHeaders(resp.Header)
+			for _, header := range endToEndHeaders {
+				cachedResp.Header[header] = resp.Header[header]
+			}
+			resp = cachedResp
+		} else if (err != nil || (cachedResp != nil && resp.StatusCode >= 500)) && req.Method == "GET" {
+			return cachedResp, nil
+		} else {
+			if err != nil || resp.StatusCode != http.StatusOK {
+				t.Cache.Delete(cacheKey)
+			}
+			if err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		resp, err = transport.RoundTrip(req)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if cacheable {
+		switch req.Method {
+		case "GET":
+			// Since the body is already uncompressed in the cache, we want to
+			// avoid the client trying to decompress it again
+			if resp.Header.Get("Content-Encoding") == "gzip" {
+				resp.Header.Del("Content-Encoding")
+			}
+			// Delay caching until EOF is reached.
+			resp.Body = &cachingReadCloser{
+				R: resp.Body,
+				OnEOF: func(r io.Reader) {
+					resp := *resp
+					resp.Body = io.NopCloser(r)
+					respBytes, err := httputil.DumpResponse(&resp, true)
+					if err == nil {
+						t.Cache.Set(cacheKey, respBytes)
+					}
+				},
+			}
+		default:
+			respBytes, err := httputil.DumpResponse(resp, true)
+			if err == nil {
+				t.Cache.Set(cacheKey, respBytes)
+			}
+		}
+	} else {
+		t.Cache.Delete(cacheKey)
+	}
+	return resp, nil
+}
+
+// varyMatches will return false unless all of the cached values for the headers listed in Vary
+// match the new request
+func varyMatches(cachedResp *http.Response, req *http.Request) bool {
+	for _, header := range headerAllCommaSepValues(cachedResp.Header, "vary") {
+		header = http.CanonicalHeaderKey(header)
+		if header != "" && req.Header.Get(header) != cachedResp.Header.Get("X-Varied-"+header) {
+			return false
+		}
+	}
+	return true
+}
+
+// headerAllCommaSepValues returns all comma-separated values (each
+// with whitespace trimmed) for header name in headers. According to
+// Section 4.2 of the HTTP/1.1 spec
+// (http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2),
+// values from multiple occurrences of a header should be concatenated, if
+// the header's value is a comma-separated list.
+func headerAllCommaSepValues(headers http.Header, name string) []string {
+	var vals []string
+	for _, val := range headers[http.CanonicalHeaderKey(name)] {
+		fields := strings.Split(val, ",")
+		for i, f := range fields {
+			fields[i] = strings.TrimSpace(f)
+		}
+		vals = append(vals, fields...)
+	}
+	return vals
+}
+
+func getEndToEndHeaders(respHeaders http.Header) []string {
+	// These headers are always hop-by-hop
+	hopByHopHeaders := map[string]struct{}{
+		"Connection":          {},
+		"Keep-Alive":          {},
+		"Proxy-Authenticate":  {},
+		"Proxy-Authorization": {},
+		"Te":                  {},
+		"Trailers":            {},
+		"Transfer-Encoding":   {},
+		"Upgrade":             {},
+	}
+
+	for _, extra := range strings.Split(respHeaders.Get("connection"), ",") {
+		// any header listed in connection, if present, is also considered hop-by-hop
+		if strings.Trim(extra, " ") != "" {
+			hopByHopHeaders[http.CanonicalHeaderKey(extra)] = struct{}{}
+		}
+	}
+	endToEndHeaders := []string{}
+	for respHeader := range respHeaders {
+		if _, ok := hopByHopHeaders[respHeader]; !ok {
+			endToEndHeaders = append(endToEndHeaders, respHeader)
+		}
+	}
+	return endToEndHeaders
+}
+
+// NewMemoryCacheTransport returns a new Transport using the in-memory cache implementation
+func NewMemoryCacheTransport(client *redis.Client, ttl time.Duration) *Transport {
+	c := NewMemoryCache(client, ttl)
+	t := NewTransport(c)
+	return t
+}
